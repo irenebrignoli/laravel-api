a664361509df760d7db22ebe90cb8eb2b36f6df5
Irene Brignoli <ire.brignoli@gmail.com>
2023-05-30 15:35:26 +0200

    rotte e controller api creati

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//rotta per API
 Route::get('/projects', [ProjectController::class, 'index']);
